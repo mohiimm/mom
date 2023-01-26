47161f179a6f0c2887b1818e0bb718a82e987d01
mohiimm <mhdsee69@gmail.com>
2023-01-26 10:51:53 +0330

    27

--- indexxx.js
+++ indexxx.js
@@ -1,1 +1,12 @@
+
 // JavaScript Document
+
+$(".default_option").click(function(){
+	$(".dropdown ul").addClass("active");
+});
+
+$(".dropdown ul li").click(function(){
+	var text = $(this).text();
+	$(".default_option").text(text);
+	$(".dropdown ul").removeClass("active");
+});
